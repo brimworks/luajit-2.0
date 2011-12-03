f3d058b149aee590f11a9533239071701c45e901
Brian Maher <brian@brimworks.com>
2011-12-03 15:52:49 -0800

    Allow bootstrapping with Lua 5.2

--- dynasm/dynasm.lua
+++ dynasm/dynasm.lua
@@ -257,6 +257,22 @@ end
 local condlevel = 0
 local condstack = {}
 
+setfenv = setfenv or function(f, t)
+                         f = (type(f) == 'function' and f or
+                          debug.getinfo(f + 1, 'f').func)
+    local name
+    local up = 0
+    repeat
+        up = up + 1
+        name = debug.getupvalue(f, up)
+    until name == '_ENV' or name == nil
+    if name then
+        debug.upvaluejoin(f, up, function() return name end, 1) -- use
+        --unique upvalue
+        debug.setupvalue(f, up, t)
+    end
+end
+
 -- Evaluate condition with a Lua expression. Substitutions already performed.
 local function cond_eval(cond)
   local func, err = loadstring("return "..cond)
